14ebdf4f2d18aebed071779f95e5d7db41821786
joecoxstrong <joecoxstrong@gmail.com>
2022-03-03 12:04:33 -0500

    created main.py and classes: customer, product, and shopping cart.

---
+++ Product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self,product_name_passed_in,price_passed_in, category_passed_in):
+        self.product_name = product_name_passed_in
+        self.price = price_passed_in
+        self.category = category_passed_in
+
+
+    
---
+++ shopping_cart.py
@@ -0,0 +1,11 @@
+class ShoppingCart:
+    def __init__(self):
+        self.products = []
+
+    def what_is_total(self):
+        #calculate and return the current total of all items in the shopping cart
+        pass
+
+    def add_new_product(self):
+        #append new product to list
+        pass
